c7725a4b8e4f208583fa8c1466b9bf9ff966aee7
Asher Yanich <ayanich@fastly.com>
2016-07-11 16:47:02 -0700

    update metadata.rb for new maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,28 @@
 name              'equivs'
-maintainer        'Mike Gardiner'
-maintainer_email  'conversationing@gmail.com'
+maintainer        'Asher Yanich'
+maintainer_email  'asher.yanich@gmail.com'
 license           'Apache 2.0'
 description       'Installs equivs and provides a recipe to set up equivs.'
 version           '0.0.3'
 
-source_url "https://github.com/gardym/#{name}"
-issues_url "https://github.com/gardym/#{name}/issues"
+source_url "https://github.com/ayanich/#{name}"
+issues_url "https://github.com/ayanich/#{name}/issues"
 
+long_description <<-EOH
+= DESCRIPTION:
+
+This installs equivs and provides a recipes to setup equivs
+
+= REQUIREMENTS:
+
+Debian or Ubuntu preferred.
+
+= NOTES:
+
+This is a fork of original author/maintainer 'Mike Gardiner'
+'conversationing@gmail.com' at "https://github.com/gardym/equivs".
+
+EOH
 depends 'apt'
 
 recipe 'equivs', 'Installs equivs'
